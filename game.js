34a163e87dcd647884116739bb3127882dce06dc
sagararathi <bravo.sagar@gmail.com>
2015-05-14 12:03:52 -0400

    Add game.js file

---
+++ game.js
@@ -0,0 +1,14 @@
+var WIDTH = 1024;
+var HEIGTH = 768;
+var xMousePosition;
+var yMousePosition;
+var xEnemyPost = 100;
+var yEnemyPost = 100;
+var xEnemySpeed = 1.5;
+var yEnemySpeed = 1.7;
+var score = 0;
+var scoreTx;
+var Timer;
+var gameTime = 0;
+var timerText;
+
